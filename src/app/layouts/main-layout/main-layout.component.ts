3cd76cb856108849c6c0fce79d7224a37514703c
Amir.Sh <mobilefactory012@gmail.com>
2026-06-28 22:12:55 +0330

    feat: create main layout and content components with responsive styles and routing integration

---
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { ToolbarComponent } from '../components/toolbar/toolbar.component';
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [ToolbarComponent, RouterOutlet],
+  templateUrl: './main-layout.component.html',
+  styleUrl: './main-layout.component.scss',
+})
+export class MainLayoutComponent {}
